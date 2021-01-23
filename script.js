60432f612016fc7e095161a44e99b03174024f55
Amanda Monteiro <a3monteiro@gmail.com>
2021-01-22 22:23:07 -0500

    landing page restyle, mobile responsive

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const app = {};
 
+app.scrollDown = () => {
+    $('#downBtn').on('click', function () {
+        $('html').animate({
+            scrollTop: $('#sampleStyles').offset().top
+        }, 1000);
+    })
+}
 
+app.navSlide = () => {
+    $('#mobileMenu').on('click', function () {
+        $('#navLinks').toggleClass('navOpen');
+        $('#mobileMenu').toggleClass('close');
+    })
+}
 
 app.init = () => {
-    
+    app.scrollDown();
+    app.navSlide();
 };
 
 $(function () {
